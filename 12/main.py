7686dd6f21d5e7f86eac07cf5e494ccfc70dd6a1
Samuel_Bohumel <samuel_bohumel@dell.com>
2025-12-16 14:25:19 +0100

    fix typo in template

--- 12/main.py
+++ 12/main.py
@@ -3,19 +3,19 @@ from loguru import logger
 import sys
 logger.remove()
 logger.add(sys.stdout, level="DEBUG")
-FILE_NAME = "input.txt"
+FILE_NAME = "ex_input.txt"
 
 def load_input(filename: str) -> list[str]:
     task_input = None
     file_path = os.path.dirname(os.path.abspath(__file__))
-    with open(os.path.join(file_path, ""), "r") as f:
+    with open(os.path.join(file_path, FILE_NAME), "r") as f:
         task_input = f.readlines()
         task_input = [row.strip() for row in task_input]
     return task_input
 
 def main():
     input_str = load_input(FILE_NAME)
-    
+    logger.info(input_str)
 
 
 if __name__ == "__main__":
