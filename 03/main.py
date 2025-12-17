3a150f8b9f94ad04815fd79bd0baa9f13c42f901
Samuel_Bohumel <samuel_bohumel@dell.com>
2025-12-17 14:49:55 +0100

    day 4 done

--- 03/main.py
+++ 03/main.py
@@ -1,9 +1,10 @@
 import os
 from loguru import logger
 import sys
+import re
 logger.remove()
 logger.add(sys.stdout, level="DEBUG")
-FILE_NAME = "ex_input.txt"
+FILE_NAME = "input.txt"
 
 def load_input(filename: str) -> list[str]:
     task_input = None
@@ -13,10 +14,46 @@ def load_input(filename: str) -> list[str]:
         task_input = [row.strip() for row in task_input]
     return task_input
 
+def extract_pattern(input_str: list[str], pattern: str) -> list[str]:
+    
+    results = []
+    for row in input_str:
+        matches = re.findall(pattern, row)
+        results.extend(matches)
+        # logger.debug(results)
+    return results
+
+
 def main():
     input_str = load_input(FILE_NAME)
-    logger.info(input_str)
-
+    # logger.info(input_str)
+    reg_muls = r'mul\([0-9]{1,3},[0-9]{1,3}\)'
+    reg_plus_instructions = r'mul\([0-9]{1,3},[0-9]{1,3}\)|do\(\)|don\'t\(\)'
+    #Task 1: extract only mul statements 
+    mul_statements = extract_pattern(input_str, reg_plus_instructions)
+    result_mul = 0
+    result_mul_plut_instr = 0
+    instruction = True
+    for statement in mul_statements:
+        if statement == "do()":
+            instruction = True
+            continue
+        if statement == "don't()":
+            instruction = False
+            continue
+        
+        numbers = statement.replace("mul(", "").replace(")", "").split(",")
+        assert len(numbers) == 2
+        #always add to res_mul
+        result_mul += (int(numbers[0]) * int(numbers[1]))
+        #add to result_mul_plut_instr only if flag is true
+        if instruction:
+            result_mul_plut_instr += (int(numbers[0]) * int(numbers[1]))
+    
+    logger.info(f"Result task1: {result_mul}")
+    logger.info(f"Result task1: {result_mul_plut_instr}")
+    
+    
 
 if __name__ == "__main__":
     main()
--- 04/main.py
+++ 04/main.py
@@ -3,9 +3,9 @@ from loguru import logger
 import sys
 logger.remove()
 logger.add(sys.stdout, level="DEBUG")
-FILE_NAME = "ex_input.txt"
+FILE_NAME = "input.txt"
 
-def load_input(filename: str) -> list[str]:
+def load_input() -> list[str]:
     task_input = None
     file_path = os.path.dirname(os.path.abspath(__file__))
     with open(os.path.join(file_path, FILE_NAME), "r") as f:
@@ -13,9 +13,78 @@ def load_input(filename: str) -> list[str]:
         task_input = [row.strip() for row in task_input]
     return task_input
 
+
+def is_position_in_map(x:int, y:int, width: int, height: int) -> bool:
+    return x >= 0 and x < width and y >= 0 and y < height
+
+
+def check_word(crossword,  word: str, positions: list[int, int]) -> bool:
+    for i in range(len(positions)):
+        if not is_position_in_map(positions[i][0], positions[i][1], len(crossword), len(crossword[i])):
+            return False
+        if word[i] != crossword[ positions[i][0]] [ positions[i][1] ]:
+            return False
+    return True
+
+
+def count_word(crossword: list[str], word: str) -> int:
+    """
+    This word search allows words to be 
+    horizontal, vertical, diagonal, written backwards, or even overlapping other words. 
+    It's a little unusual, though, as you don't merely need to find 
+    one instance of XMAS - you need to find all of them.
+    """
+    word_count = 0
+    for i in range(len(crossword)):
+        for j in range(len(crossword[i])):
+            # if frist leter match, check all directions
+            if crossword[i][j] == word[0]:
+                #check all directions
+                directions = [
+                    [ [i+k, j   ] for k in range(len(word)) ],
+                    [ [i+k, j+k ] for k in range(len(word)) ],
+                    [ [i+k, j-k ] for k in range(len(word)) ],
+                    [ [i-k, j   ] for k in range(len(word)) ],
+                    [ [i-k, j+k ] for k in range(len(word)) ],
+                    [ [i-k, j-k ] for k in range(len(word)) ],
+                    [ [i,   j+k ] for k in range(len(word)) ],
+                    [ [i,   j-k ] for k in range(len(word)) ],
+                ]
+                word_count += sum([ check_word(crossword, word, direction) for direction in directions]) 
+    return word_count
+
+
+def count_xmas_in_x_shape(crossword: list[str]) -> int:
+    word_count = 0
+    height = len(crossword)
+    width = len(crossword[0])
+    for i in range(len(crossword)):
+        for j in range(len(crossword[i])):
+            if crossword[i][j] == "A":
+                # check if cross is in bounds 
+                if  is_position_in_map(i+1, j+1, width, height) and \
+                    is_position_in_map(i+1, j-1, width, height) and \
+                    is_position_in_map(i-1, j+1, width, height) and \
+                    is_position_in_map(i-1, j-1, width, height):
+                        #check if there is MAS on each direction
+                        if (crossword[i-1][j-1] == "M" and crossword[i+1][j+1] == "S" or   
+                            crossword[i-1][j-1] == "S" and crossword[i+1][j+1] == "M") and \
+                            (crossword[i+1][j-1] == "M" and crossword[i-1][j+1] == "S" or 
+                             crossword[i+1][j-1] == "S" and crossword[i-1][j+1] == "M"):
+                                word_count += 1
+    return word_count
+                        
+                    
+
+
 def main():
-    input_str = load_input(FILE_NAME)
-    logger.info(input_str)
+    input_str = load_input()
+    # logger.info(input_str)
+    
+    word_count = count_word(crossword=input_str, word="XMAS")
+    logger.info(f"XMAS in crossword: {word_count}")
+    xmas_cross_count = count_xmas_in_x_shape(input_str)
+    logger.info(f"XMAS cross in crossword: {xmas_cross_count}")
 
 
 if __name__ == "__main__":
